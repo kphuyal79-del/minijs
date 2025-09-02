5f622353a327575adc7a2bb88ec2bf75af3a0d38
kphuyal79-del <kphuyal79@gmail.com>
2025-09-02 20:52:21 +0545

    clock

--- clock/clock.js
+++ clock/clock.js
@@ -4,12 +4,17 @@
     const minute = now.getMinutes();
     const hours = now.getHours();
 
-    const secAngle = second * 6; 
-    const minAngle = minute * 6
-    const hrAngle = hours*30 
+    const secAngle = second * 6 ; 
+    const minAngle = minute * 6 + second*0.1
+    const hrAngle = (hours%12)*30 + minute*0.1
 
     document.getElementById("sec").style.transform = `rotate(${secAngle}deg)`;
     document.getElementById("min").style.transform = `rotate(${minAngle}deg)`;
     document.getElementById("hour").style.transform = `rotate(${hrAngle}deg)`;
+
+    document.getElementById('d-sec').textContent = `${second}`
+    document.getElementById('d-min').textContent = `${minute}:`
+    document.getElementById('d-hrs').textContent = hours<10?`0${hours}`:`${hours}`
   }
   setInterval(updateClock, 1000);
+  updateClock()
